08c53b2cac085172d1c8c459b445f1beb77051b7
Adrian Bustamante <arkadwn@gmail.com>
2017-05-21 16:45:27 -0500

    Creaci´n de script para la creación de la BDD. Ejecutar con permisos superiores.

---
+++ createUserCAA.sql
@@ -0,0 +1,5 @@
+create user 'usercaa'@'localhost' identified by 'arkadwn1';
+Grant insert on caa.* to 'usercaa'@'localhost';
+Grant select on caa.* to 'usercaa'@'localhost';
+Grant delete on caa.* to 'usercaa'@'localhost';
+Grant update on caa.* to 'usercaa'@'localhost';
